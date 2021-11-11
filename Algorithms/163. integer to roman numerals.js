3435ba83f9227c43c2225236b5b8a38f21b261a7
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-11-11 23:26:09 +0900

    Solved 162

--- Algorithms/163. integer to roman numerals.js
+++ Algorithms/163. integer to roman numerals.js
@@ -1,5 +1,33 @@
 //first try
-const valueMap = {
+function romanToInteger(str) {
+  const map = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+  }
+
+  let sum = 0
+
+  for (let i = 0; i < str.length; i++) {
+    const sign = map[str[i]] < map[str[i+1]] ? -1 : 1
+
+    sum += map[str[i]] * sign
+  }
+
+  return sum
+}
+
+console.log(romanToInteger('MMMCDXX'))
+
+
+
+  //second try
+
+  const valueMap = {
     I: 1,
     V: 5,
     X: 10,
@@ -12,37 +40,11 @@ const valueMap = {
   function romanToInteger(str) {
      let result = 0
      for (let i = 0; i < str.length; i++) {
-       const val = valueMap[str[i]]
-       const nexVal = i < str.length - 1 ? valueMap[str[i + 1]] : undefined
+       let val = valueMap[str[i]]
+       let nextVal = i < str.length - 1 ? valueMap[str[i + 1]] : undefined
        
-       const sign = nexVal !== undefined && nexVal > val ? -1 : 1
+       let sign = nextVal !== undefined && nextVal > val ? -1 : 1
        result += sign * val
      }
      return result
   }
-
-
-  //second try
-  function romanToInteger(str) {
-    const map = {
-      'I': 1,
-      'V': 5,
-      'X': 10,
-      'L': 50,
-      'C': 100,
-      'D': 500,
-      'M': 1000,
-    }
-  
-    let sum = 0
-  
-    for (let i = 0; i < str.length; i++) {
-      const sign = map[str[i]] < map[str[i+1]] ? -1 : 1
-  
-      sum += map[str[i]] * sign
-    }
-  
-    return sum
-  }
-  
-  console.log(romanToInteger('MMMCDXX'))
--- BasicJS.js
+++ BasicJS.js
@@ -8,3 +8,19 @@ a1.concat(a2)
 a1.join(",")
 
 a1.slice(start, end)
+
+// splice
+a1.splice(start, deleteCount, items)
+// https://www.w3schools.com/jsref/jsref_splice.asp
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+// At position 2, add 2 elements: 
+fruits.splice(2, 0, "Lemon", "Kiwi");
+// At position 2, add new items, and remove 1 item:
+fruits.splice(2, 1, "Lemon", "Kiwi");
+// At position 2, remove 2 items:
+fruits.splice(2, 2);
+
+
+
+// https://alligator.io/js/push-pop-shift-unshift-array-methods/
+//push pop unshift shift
