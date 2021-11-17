3fed0f95fa2cd8101ff469d22668f2a0f06ab6aa
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-11-17 22:22:49 +0900

    Added a second approach to 155

--- Algorithms/155. create a count function.js
+++ Algorithms/155. create a count function.js
@@ -1,9 +1,32 @@
 //Please create a function count(), when called it should return 
 //how many times it has been called, count.reset() should also implemented.
 // your code here
-const count = (() => {
+
+// this is a self-executing anonymous function where you define a function and force executing it immediately.
+const count = (
+    () => {
     let num = 0;
     const func = () => ++num;
     func.reset = () => num = 0;
     return func;
-  })();
+  }
+  )();
+  
+
+  //Below does NOT work properly
+  let count = function(){
+    let num = 0;
+    let func = () => ++num;
+    func.reset = () => num = 0;
+    return func;
+  }
+
+  //second approach
+  let counter=0;
+  
+ let count = function(){
+  return ++counter;
+}
+count.reset = function(){
+  counter=0;
+}
