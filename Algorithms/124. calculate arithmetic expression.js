89ed33c1d81997c5bc7dcd7c0a9b7e2f48b73238
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-11-20 16:44:20 +0900

    Solved 136

---
+++ Algorithms/124. calculate arithmetic expression.js
@@ -0,0 +1,12 @@
+// In 119. create a tokenizer, you are able to extract the tokens from a string with invalid spaces.
+// Now please calculate() the result of the string. You can use the tokenizer you wrote before.
+
+calculate('1 * (20 - 300 ) ')
+// -280
+calculate(' 1/0 ')
+// Infinity
+
+// the input expression is syntactically valid, 
+// containing non-negative integers, +, -, *, /, (, ) and spaces
+// Don't use eval()
+
---
+++ Algorithms/136. find median of two sorted array.js
@@ -0,0 +1,26 @@
+
+/**
+ * @param {number[]} arr1 - sorted integer array
+ * @param {number[]} arr2 - sorted integer array
+ * @returns {number}
+ */
+ function median(arr1, arr2) {
+    let sortedArr = arr1.concat(arr2).sort((a,b)=>a-b)
+    // let median = ""
+    if(sortedArr.length % 2 !== 0){
+     while(sortedArr.length > 3){
+       sortedArr.pop()
+       sortedArr.shift()
+     } return sortedArr[1]
+    } 
+   else {
+       while(sortedArr.length > 4){
+       sortedArr.pop()
+       sortedArr.shift()
+       }
+   return (sortedArr[1]+sortedArr[2])/2
+     }
+  }
+  console.log(median([1,2],[3,4,5]))
+  console.log(median([1,2],[3,4]))
+  
